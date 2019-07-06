a67d02b75db1c5d2d9064b17961428737ceabbfa
juliobana <47338088+juliobana@users.noreply.github.com>
2019-07-05 21:34:27 -0300

    Add files via upload

---
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -0,0 +1,20 @@
+
+function mostrar()
+
+/*Realizar el algoritmo que pida el ancho y el largo de un rectángulo 
+por prompt y que muestre el perímetro por alert.*/
+{
+
+var largo;
+var ancho;
+var perimetro;
+
+largo = prompt("Ingrese el Lado");
+ancho = prompt("Ingrese el Ancho");
+perimetro = largo * ancho;
+alert("El perimetro es " + perimetro);
+
+
+
+
+}
